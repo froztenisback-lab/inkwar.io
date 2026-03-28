8c46d059e74bdd3e05b2ccce88a4cacd329f9d34
froztenisback-lab <froztenisback@gmail.com>
2026-03-28 13:51:57 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Paintblitz.io/', 
+  base: '/', // This ensures it looks in the root for files
 })
