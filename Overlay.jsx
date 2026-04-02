a5931b26d6a7a6166fcfc18833b01f3a2d549631
froztenisback-lab <froztenisback@gmail.com>
2026-04-02 14:19:27 +0700

    Update Overlay.jsx

--- Overlay.jsx
+++ Overlay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Overlay({bg,icon,title,sub,btnColor,onBack}){
   return(
-    <div className={`fixed inset-0 w-full h-full ${bg} backdrop-blur-md flex items-center justify-center p-4 z-[300]`}>
+    <div className={`fixed inset-0 w-full h-full ${bg} backdrop-blur-md flex items-center justify-center p-4 z-[300]`} style="position: fixed;">
       <div className="text-center">{icon}
         <h2 className="text-4xl font-black text-white mb-2">{title}</h2>
         <p className="text-white/70 mb-6 font-medium">{sub}</p>
